f37d8e1fd8b2b9cd3699987940db6204125607f9
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-04 21:27:28 +0700

    operator aritmatika

---
+++ tugas11.js
@@ -0,0 +1,5 @@
+//Gunakan lah 4 angka ini 2, 4, 5 dan 10 sehingga menghasilkan output 25
+// perhitungan dengan penjumlahan (+) , perkalian (*) dan pembagian (/)
+var result = (4 - 2) * 10 + 5;
+
+console.log(result);
